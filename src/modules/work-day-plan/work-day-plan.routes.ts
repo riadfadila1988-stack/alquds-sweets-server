396bab3439e04fb8f58fd0df5d74df0ba344aa41
riadfadila <riad.faila.1988@gmail.com>
2025-11-27 12:35:21 +0200

    work day plan permissions

--- src/modules/work-day-plan/work-day-plan.routes.ts
+++ src/modules/work-day-plan/work-day-plan.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import WorkDayPlanController from './work-day-plan.controller';
 import { Request, Response } from 'express';
+import {authenticateJWT, authorizeRoles} from "../../core/middlewares/authJwt";
 
 const router = Router();
 
 router.get('/by-date', async (req: Request, res: Response) => await WorkDayPlanController.getByDate(req, res));
 router.get('/by-date-for-user', async (req: Request, res: Response) => await WorkDayPlanController.getByDateForUser(req, res));
 router.get('/', async (req: Request, res: Response) => await WorkDayPlanController.getAll(req, res));
-router.post('/', async (req: Request, res: Response) => await WorkDayPlanController.createOrUpdate(req, res));
+router.post('/', authenticateJWT, authorizeRoles('admin'), async (req: Request, res: Response) => await WorkDayPlanController.createOrUpdate(req, res));
 router.post('/update-user-task', async (req: Request, res: Response) => await WorkDayPlanController.updateUserTask(req, res));
 router.delete('/:id', async (req: Request, res: Response) => await WorkDayPlanController.delete(req, res));
 
